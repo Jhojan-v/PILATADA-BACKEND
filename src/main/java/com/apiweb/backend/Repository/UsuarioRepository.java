e702748bf3e241a9ff954f12c129e33e2b281c7e
isajar1 <sarai.isajar@uao.edu.co>
2026-03-28 12:38:19 -0500

    Sync pirated backend with Gestion Salud base

--- src/main/java/com/apiweb/backend/Repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apiweb.backend.Repository;
-
-import com.apiweb.backend.Model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
